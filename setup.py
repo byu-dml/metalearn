923502a4aa492a3d52a8c10aec76b670e0ebe807
Casey Davis <caseykdavis@gmail.com>
2017-09-06 14:15:39 -0600

    Adds some basic setup files which will be useful

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'metafeaturizer',
+  packages = ['metafeaturizer'],
+  version = '0.1',
+  description = 'A package to retrieve metafeatures from datasets',
+  author = 'Christophe Giraud-Carrier',
+  author_email = 'cgc@cs.byu.edu',
+  url = 'https://github.com/byu-dml/metafeature_summary',
+  download_url = 'https://github.com/byu-dml/metafeature_summary/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['metalearning', 'machine learning', 'darpa'],
+  classifiers = [],
+)
