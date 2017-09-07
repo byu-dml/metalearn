42fb041f67961d06c1dbeea035380fd4a8e2b29e
Casey Davis <caseykdavis@gmail.com>
2017-09-07 16:45:27 -0600

    Updates the version again for PyPI

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 setup(
   name = 'metalearn',
   packages = find_packages(),
-  version = '0.1.1',
+  version = '0.1.2',
   description = 'A package to retrieve metafeatures from datasets',
   author = 'Casey Davis',
   author_email = 'caseykdavis@gmail.com',
   url = 'https://github.com/byu-dml/metalearn',
-  download_url = 'https://github.com/byu-dml/metalearn/archive/0.1.tar.gz',
+  download_url = 'https://github.com/byu-dml/metalearn/archive/0.1.2.tar.gz',
   keywords = ['metalearning', 'machine learning', 'darpa', 'metalearn'],
   classifiers = [],
 )
