e4878640955b58247a77b55ace01e681a253850a
Casey Davis <caseykdavis@gmail.com>
2017-09-07 12:29:36 -0600

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
   packages = ['metalearn'],
   version = '0.1',
   description = 'A package to retrieve metafeatures from datasets',
-  author = 'Christophe Giraud-Carrier',
-  author_email = 'cgc@cs.byu.edu',
+  author = 'Casey Davis',
+  author_email = 'caseykdavis@gmail.com',
   url = 'https://github.com/byu-dml/metalearn',
-  download_url = 'https://github.com/byu-dml/metalearn/archive/0.1.tar.gz', # I'll explain this in a second
-  keywords = ['metalearning', 'machine learning', 'darpa'],
+  download_url = 'https://github.com/byu-dml/metalearn/archive/0.1.tar.gz',
+  keywords = ['metalearning', 'machine learning', 'darpa', 'metalearn'],
   classifiers = [],
 )
