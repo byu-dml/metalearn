326e7feaff741e636b07b0c0225a725572c7a067
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2018-02-12 15:24:11 -0700

    moved openml metafeature map to json file; formatting

--- test_metafeatures.py
+++ test_metafeatures.py
@@ -33,67 +33,19 @@ def import_openml_dataset(id=4):
 
     return dataframe, omlMetafeatures
 
-def compare_with_openml(dataframe,omlMetafeatures):
+def compare_with_openml(dataframe, omlMetafeatures):
     # get metafeatures from dataset using our metafeatures
     ourMetafeatures = extract_metafeatures(dataframe)
-
-    mfDict ={}
-    mfDict['EquivalentNumberOfAtts'] = 'EquivalentNumberOfFeatures', 1
-    mfDict['MeanCardinalityOfNominalAttributes'] = 'MeanCardinalityOfNominalFeatures', 1
-    mfDict['StdevCardinalityOfNominalAttributes'] = 'StdevCardinalityOfNominalFeatures', 1
-    mfDict['MaxCardinalityOfNominalAttributes'] = 'MaxCardinalityOfNominalFeatures', 1
-    mfDict['MinCardinalityOfNominalAttributes'] = 'MinCardinalityOfNominalFeatures', 1
-    mfDict['MeanCardinalityOfNumericAttributes'] = 'MeanCardinalityOfNumericFeatures', 1
-    mfDict['StdevCardinalityOfNumericAttributes'] = 'StdevCardinalityOfNumericFeatures', 1
-    mfDict['MaxCardinalityOfNumericAttributes'] = 'MaxCardinalityOfNumericFeatures', 1
-    mfDict['MinCardinalityOfNumericAttributes'] = 'MinCardinalityOfNumericFeatures', 1
-    mfDict['MinorityClassPercentage'] = 'MinClassProbability', 100
-    mfDict['MajorityClassPercentage'] = 'MaxClassProbability', 100
-    mfDict['MaxMeansOfNumericAtts'] = 'MaxMeansOfNumericFeatures', 1
-    mfDict['MaxStdevOfNumericAtts'] = 'MaxStdevOfNumericFeatures', 1
-    mfDict['MaxSkewnessOfNumericAtts'] = 'MaxSkewnessOfNumericFeatures', 1
-    mfDict['MaxKurtosisOfNumericAtts'] = 'MaxKurtosisOfNumericFeatures', 1
-    mfDict['MinMeansOfNumericAtts'] = 'MinMeansOfNumericFeatures', 1
-    mfDict['MinStdevOfNumericAtts'] = 'MinStdevOfNumericFeatures', 1
-    mfDict['MinSkewnessOfNumericAtts'] = 'MinSkewnessOfNumericFeatures', 1
-    mfDict['MinKurtosisOfNumericAtts'] = 'MinKurtosisOfNumericFeatures', 1
-    mfDict['MeanMeansOfNumericAtts'] = 'MeanMeansOfNumericFeatures', 1
-    mfDict['MeanStdevOfNumericAtts'] = 'MeanStdevOfNumericFeatures', 1
-    mfDict['MeanSkewnessOfNumericAtts'] = 'MeanSkewnessOfNumericFeatures', 1
-    mfDict['MeanKurtosisOfNumericAtts'] = 'MeanKurtosisOfNumericFeatures', 1
-    mfDict['StdevMeansOfNumericAtts'] = 'StdevMeansOfNumericFeatures', 1
-    mfDict['StdevStdevOfNumericAtts'] = 'StdevStdevOfNumericFeatures', 1
-    mfDict['StdevSkewnessOfNumericAtts'] = 'StdevSkewnessOfNumericFeatures', 1
-    mfDict['StdevKurtosisOfNumericAtts'] = 'StdevKurtosisOfNumericFeatures', 1
-    mfDict['Quartile1MeansOfNumericAtts'] = 'Quartile1MeansOfNumericFeatures', 1
-    mfDict['Quartile1StdevOfNumericAtts'] = 'Quartile1StdevOfNumericFeatures', 1
-    mfDict['Quartile1SkewnessOfNumericAtts'] = 'Quartile1SkewnessOfNumericFeatures', 1
-    mfDict['Quartile1KurtosisOfNumericAtts'] = 'Quartile1KurtosisOfNumericFeatures', 1
-    mfDict['Quartile2MeansOfNumericAtts'] = 'Quartile2MeansOfNumericFeatures', 1
-    mfDict['Quartile2StdevOfNumericAtts'] = 'Quartile2StdevOfNumericFeatures', 1
-    mfDict['Quartile2SkewnessOfNumericAtts'] = 'Quartile2SkewnessOfNumericFeatures', 1
-    mfDict['Quartile2KurtosisOfNumericAtts'] = 'Quartile2KurtosisOfNumericFeatures', 1
-    mfDict['Quartile3MeansOfNumericAtts'] = 'Quartile3MeansOfNumericFeatures', 1
-    mfDict['Quartile3StdevOfNumericAtts'] = 'Quartile3StdevOfNumericFeatures', 1
-    mfDict['Quartile3SkewnessOfNumericAtts'] = 'Quartile3SkewnessOfNumericFeatures', 1
-    mfDict['Quartile3KurtosisOfNumericAtts'] = 'Quartile3KurtosisOfNumericFeatures', 1
-    mfDict['PercentageOfInstancesWithMissingValues'] = 'RatioOfInstancesWithMissingValues', 100
-    mfDict['PercentageOfMissingValues'] = 'RatioOfMissingValues', 100
-    mfDict['PercentageOfNumericFeatures'] = 'RatioOfNumericFeatures', 100
-    mfDict['PercentageOfSymbolicFeatures'] = 'RatioOfNominalFeatures', 100
-
-
-    # print(json.dumps(ourMetafeatures, sort_keys=True, indent=4))
-    # print(json.dumps(omlMetafeatures, sort_keys=True, indent=4))
-
+    # todo use nested dictionary instead of tuple to make values more descriptive
+    mfDict = json.load(open("oml_metafeature_map.json", "r"))
 
     omlExclusiveMf = {}
     ourExclusiveMf = ourMetafeatures
-    sharedMf=[]
+    sharedMf = []
     sharedMf.append(("OML Metafeature Name", "OML Metafeature Value", "Our Metafeature Name", "Our Metafeature Value", "Similar?"))
     for omlMetafeature in omlMetafeatures :
         # compare shared metafeatures
-        if (ourMetafeatures.get(omlMetafeature) != None 
+        if (ourMetafeatures.get(omlMetafeature) != None
             or ourMetafeatures.get("" if omlMetafeature not in mfDict else mfDict.get(omlMetafeature)[0]) != None) :
             omlMetafeatureName= ""
             omlMetafeatureValue= ""
@@ -152,16 +104,6 @@ def compare_with_openml(dataframe,omlMetafeatures):
     print("\nMetafeatures calculated by our primitive exclusively:")
     print(json.dumps(ourExclusiveMf, sort_keys=True, indent=4))
 
-        
-        
-
-
-
-
-
-
-
-
 def load_arff(infile_path):
     f = open(infile_path)
     dataframe = a2p.load(f)
@@ -209,8 +151,6 @@ def main():
 
 if __name__ == "__main__":
     # print(compute_metafeatures("./iris.arff"))
-    print("\n\n\n")
     dataframe, omlMetafeatures = import_openml_dataset()
     compare_with_openml(dataframe,omlMetafeatures)
     main()
-
