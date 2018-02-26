5198e861f60c2c8042ec99f77725ff0a2b40680e
poolguy <sethd63@gmail.com>
2018-02-26 11:32:08 -0700

    alter mfnamedict and update usage

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,9 @@
-from test.metalearn.metafeatures.test_metafeatures import main as mf_tests
+from test.metalearn.metafeatures.test_metafeatures import main as mf_tests, import_openml_dataset, compare_with_openml
 
 def main():
-    mf_tests()
+    # mf_tests()
+    dataframe, omlMetafeatures = import_openml_dataset()
+    compare_with_openml(dataframe, omlMetafeatures)
 
 if __name__ == '__main__':
     main()
--- test/metalearn/metafeatures/test_metafeatures.py
+++ test/metalearn/metafeatures/test_metafeatures.py
@@ -16,7 +16,8 @@ def import_openml_dataset(id=4):
     # get a dataset from openml using a dataset id
     dataset = openml.datasets.get_dataset(id)
     # get the metafeatures from the dataset
-    omlMetafeatures = dataset.qualities
+    omlMetafeatures = {x: float(v) for x, v in dataset.qualities.items()}
+
     # get X, Y, and attributes from the dataset
     X, Y, attributes = dataset.get_data(target=dataset.default_target_attribute, return_attribute_names=True)
 
@@ -25,74 +26,62 @@ def import_openml_dataset(id=4):
     dataframe = dataframe.assign(target=pd.Series(Y))
 
     # format attributes
+    # TODO: find out if pandas infers type correctly (remove this code after)
     for i in range(len(X[0])):
         attributes[i] = (attributes[i], str(type(X[0][i])))
         # set types of attributes (column headers) as well as the names
 
     return dataframe, omlMetafeatures
 
+
 def compare_with_openml(dataframe, omlMetafeatures):
     # get metafeatures from dataset using our metafeatures
     ourMetafeatures = extract_metafeatures(dataframe)
-    # todo use nested dictionary instead of tuple to make values more descriptive
-    mfDict = json.load(open("oml_metafeature_map.json", "r"))
+    mfNameMap = json.load(open("../../../oml_metafeature_map.json", "r"))
 
-    omlExclusiveMf = {}
-    ourExclusiveMf = ourMetafeatures
-    sharedMf = []
-    sharedMf.append(("OML Metafeature Name", "OML Metafeature Value", "Our Metafeature Name", "Our Metafeature Value", "Similar?"))
-    for omlMetafeature in omlMetafeatures :
-        # compare shared metafeatures
-        if (ourMetafeatures.get(omlMetafeature) != None
-            or ourMetafeatures.get("" if omlMetafeature not in mfDict else mfDict.get(omlMetafeature)[0]) != None) :
-            omlMetafeatureName= ""
-            omlMetafeatureValue= ""
-            ourMetafeatureName= ""
-            ourMetafeatureValue= ""
-            similarityString = ""
-            diff = 0
-            similarityQualifier = 0.05
+    omlExclusiveMf = {x: v for x, v in omlMetafeatures.items()}
+    ourExclusiveMf = {}
+    sharedMfList = []
+    sharedMf = pd.DataFrame(columns=(
+    "OML Metafeature Name", "OML Metafeature Value", "Our Metafeature Name", "Our Metafeature Value", "Similar?"))
 
-            # compare metafeatures with the same name
-            if (ourMetafeatures.get(omlMetafeature) != None):
-                omlMetafeatureName = omlMetafeature
-                omlMetafeatureValue = float(omlMetafeatures.get(omlMetafeature))
-                ourMetafeatureName = omlMetafeature
-                ourMetafeatureValue = float(ourMetafeatures.get(ourMetafeatureName))
-                # similarityQualifier = omlMetafeatureValue * .05
-                diff = omlMetafeatureValue - ourMetafeatureValue
-            # compare equivalent metafeatures with different names
-            elif (ourMetafeatures.get(mfDict.get(omlMetafeature)[0]) != None):
-                ourMetafeatureName, multiplier = mfDict.get(omlMetafeature)
-                ourMetafeatureValue = float(ourMetafeatures.get(ourMetafeatureName))
-                omlMetafeatureName = omlMetafeature
-                omlMetafeatureValue = float(omlMetafeatures.get(omlMetafeature))
-                # similarityQualifier = omlMetafeatureValue * .05
-                diff = omlMetafeatureValue - (ourMetafeatureValue * multiplier)
-
-            # determine if the metafeatures are similar
-            if (abs(diff) <= similarityQualifier):
-                similarityString = "yes"
+    for metafeatureName, metafeatureValue in ourMetafeatures.items():
+        if mfNameMap.get(metafeatureName) is None:
+            ourExclusiveMf[metafeatureName] = metafeatureValue
+        else:
+            openmlName = mfNameMap[metafeatureName]["openmlName"]
+            if omlMetafeatures.get(openmlName) is None:
+                ourExclusiveMf[metafeatureName] = metafeatureValue
             else:
-                similarityString = "no"
+                diff = abs(omlMetafeatures[openmlName] - metafeatureValue)
+                if diff > .05:
+                    similarityString = "No"
+                else:
+                    similarityString = "Yes"
 
-            sharedMf.append((omlMetafeatureName, str(omlMetafeatureValue), ourMetafeatureName, str(ourMetafeatureValue), similarityString))
-            del ourExclusiveMf[ourMetafeatureName]
-        # Grab metafeatures computed by OpenML and not us
-        else :
-            omlExclusiveMf[omlMetafeature] = omlMetafeatures[omlMetafeature]
+                sharedMfList.append([openmlName, omlMetafeatures[openmlName], metafeatureName, metafeatureValue, similarityString])
+                # sharedMf.append(pd.Series(
+                #     (openmlName, omlMetafeatures[openmlName], metafeatureName, metafeatureValue, similarityString)),
+                #                 ignore_index=True)
+                # remove shared metafeature from omlExclusiveMf
+                omlExclusiveMf.pop(openmlName)
 
+    # sharedMf = pd.concat([pd.DataFrame(sharedMfList[i]) for i in sharedMfList], ignore_index=True)
+    for i in sharedMfList:
+        sharedMf.loc[i] = [pd.DataFrame(sharedMfList[i][n]) for n in sharedMfList[i]]
 
     # print shared metafeature comparison
     print("Shared metafeature comparison")
-    print("{0:40} {1:30} {2:40} {3:30} {4:5}".format(sharedMf[0][0],sharedMf[0][1],sharedMf[0][2],sharedMf[0][3],sharedMf[0][4]).rjust(3))
-    for x in sharedMf :
-        if (x[4] == "yes") :
-            print("{0:40} {1:30} {2:40} {3:30} {4:5}".format(x[0],x[1],x[2],x[3],x[4]).rjust(3))
-
-    for x in sharedMf :
-        if (x[4] == "no") :
-            print("{0:40} {1:30} {2:40} {3:30} {4:5}".format(x[0],x[1],x[2],x[3],x[4]).rjust(3))
+    # print("{0:40} {1:30} {2:40} {3:30} {4:5}".format(sharedMf[0][0],sharedMf[0][1],sharedMf[0][2],sharedMf[0][3],sharedMf[0][4]).rjust(3))
+    # for x in sharedMf :
+    #     if (x[4] == "yes") :
+    #         print("{0:40} {1:30} {2:40} {3:30} {4:5}".format(x[0],x[1],x[2],x[3],x[4]).rjust(3))
+    #
+    # for x in sharedMf :
+    #     if (x[4] == "no") :
+    #         print("{0:40} {1:30} {2:40} {3:30} {4:5}".format(x[0],x[1],x[2],x[3],x[4]).rjust(3))
+    # with pd.option_context('display.max_rows', None, 'display.max_columns', 3):
+    print(sharedMf)
 
     # print metafeatures calculated only by OpenML
     print("\nMetafeatures calculated by OpenML exclusively:")
@@ -102,12 +91,14 @@ def compare_with_openml(dataframe, omlMetafeatures):
     print("\nMetafeatures calculated by our primitive exclusively:")
     print(json.dumps(ourExclusiveMf, sort_keys=True, indent=4))
 
+
 def load_arff(infile_path):
     f = open(infile_path)
     dataframe = a2p.load(f)
     column_name = [name for name in list(dataframe.columns) if 'class@' in name][0]
     dataframe = dataframe.rename(index=str, columns={column_name: 'target'})
     return dataframe
+
 
 def extract_metafeatures(dataframe):
     metafeatures = {}
@@ -125,12 +116,14 @@ def extract_metafeatures(dataframe):
         metafeatures[feature] = features_df[feature].as_matrix()[0]
     return metafeatures
 
+
 def sort_by_compute_time(metafeatures):
     metafeature_times = {}
     for key in metafeatures:
         if "_Time" in key:
             metafeature_times[key] = metafeatures[key]
     return dict(sorted(metafeature_times.items(), key=lambda x: x[1], reverse=True))
+
 
 def main():
     # todo compare computed metafeatures against a static file of pre-computed metafeatures
@@ -139,7 +132,7 @@ def main():
     # we then manually decide which metafeatures are correct and update the static file as needed
     datasets = json.load(open("./data/all_datasets.json", "r"))
     for obj in datasets:
-        filename = "./data/"+obj["path"]
+        filename = "./data/" + obj["path"]
         target_name = obj["target_name"]
         print(filename)
         ext = filename.split(".")[-1]
@@ -163,7 +156,9 @@ def main():
         # print(len(metafeatures), "metafeatures")
     # print("tests finished")
 
+
 if __name__ == "__main__":
-    # dataframe, omlMetafeatures = import_openml_dataset()
-    # compare_with_openml(dataframe,omlMetafeatures)
-    main()
+    # TODO: pass in oml dataset id from command line
+    dataframe, omlMetafeatures = import_openml_dataset()
+    compare_with_openml(dataframe, omlMetafeatures)
+    # main()
