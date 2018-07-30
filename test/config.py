544aa994c98cdad143891b7ae89a2617eb65b400
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2018-07-30 15:11:21 -0600

    replaced single quotes with double

--- test/config.py
+++ test/config.py
@@ -4,5 +4,5 @@ suite.
 """
 
 CORRECTNESS_SEED = 0
-METADATA_PATH = './test/data/test_dataset_metadata.json'
-METAFEATURES_DIR = './test/data/dataset_metafeatures/'
+METADATA_PATH = "./test/data/test_dataset_metadata.json"
+METAFEATURES_DIR = "./test/data/dataset_metafeatures/"
--- test/data/compute_dataset_metafeatures.py
+++ test/data/compute_dataset_metafeatures.py
@@ -17,21 +17,21 @@ def extract_metafeatures(X, Y, seed=None):
     return metafeatures
 
 def get_dataset_metafeatures_path(dataset_filename):
-    dataset_name = dataset_filename.rsplit('.', 1)[0]
-    return os.path.join(METAFEATURES_DIR, dataset_name+'_mf.json')
+    dataset_name = dataset_filename.rsplit(".", 1)[0]
+    return os.path.join(METAFEATURES_DIR, dataset_name+"_mf.json")
 
 def compute_dataset_metafeatures():
-    metadata = json.load(open(METADATA_PATH, 'r'))
+    metadata = json.load(open(METADATA_PATH, "r"))
     for dataset_metadata in metadata:
-        dataset_filename = dataset_metadata['filename']
-        target_class_name = dataset_metadata['target_class_name']
-        index_col_name = dataset_metadata.get('index_col_name', None)
+        dataset_filename = dataset_metadata["filename"]
+        target_class_name = dataset_metadata["target_class_name"]
+        index_col_name = dataset_metadata.get("index_col_name", None)
 
         choice = None
-        while not choice in ['y', 'v', 'n']:
-            choice = input(dataset_filename + ' [(y)es, (v)erbose, (n)o]: ')
+        while not choice in ["y", "v", "n"]:
+            choice = input(dataset_filename + " [(y)es, (v)erbose, (n)o]: ")
 
-        if choice == 'n':
+        if choice == "n":
             continue
 
         X, Y, column_types = read_dataset(dataset_filename, index_col_name, target_class_name)
@@ -40,23 +40,23 @@ def compute_dataset_metafeatures():
         metafeatures = extract_metafeatures(X=X, Y=Y, seed=CORRECTNESS_SEED)
         run_time = time.time() - start_time
 
-        # metafeatures = {key: value for key, value in metafeatures.items() if not '_Time' in key}
+        # metafeatures = {key: value for key, value in metafeatures.items() if not "_Time" in key}
 
         for key, value in metafeatures.items():
-            if 'int' in str(type(value)):
+            if "int" in str(type(value)):
                 metafeatures[key] = int(value)
-            elif 'float' in str(type(value)):
+            elif "float" in str(type(value)):
                 metafeatures[key] = float(value)
             else:
-                raise Exception('unhandled type: {}'.format(type(value)))
+                raise Exception("unhandled type: {}".format(type(value)))
 
-        if choice == 'v':
+        if choice == "v":
             print(json.dumps(metafeatures, sort_keys=True, indent=4))
-        print('Runtime: ' + str(run_time))
+        print("Runtime: " + str(run_time))
 
         choice = None
-        while not choice in ['y', 'n']:
-            choice = input(f'Update {dataset_filename} metafeatures? [(y)es, (n)o]: ')
-        if choice == 'y':
+        while not choice in ["y", "n"]:
+            choice = input(f"Update {dataset_filename} metafeatures? [(y)es, (n)o]: ")
+        if choice == "y":
             mf_file_path = get_dataset_metafeatures_path(dataset_filename)
-            json.dump(metafeatures, open(mf_file_path, 'w'), sort_keys=True, indent=4)
+            json.dump(metafeatures, open(mf_file_path, "w"), sort_keys=True, indent=4)
