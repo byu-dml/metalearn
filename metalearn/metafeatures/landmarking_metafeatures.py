0a04e109c1126215e3af0a99feb55ccb19996ed2
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2018-08-06 16:43:43 -0600

    made run_pipeline more general

--- metalearn/metafeatures/landmarking_metafeatures.py
+++ metalearn/metafeatures/landmarking_metafeatures.py
@@ -11,9 +11,6 @@ from sklearn.tree import DecisionTreeClassifier
 from .common_operations import *
 
 
-import warnings
-warnings.filterwarnings("ignore", category=UserWarning) # suppress sklearn warnings
-
 '''
 
 Compute Landmarking meta-features according to Reif et al. 2012.
@@ -23,39 +20,51 @@ Decision Node, Random Node.
 
 '''
 
-def run_pipeline(X, Y, estimator, n_folds, seed):
-    pipe = Pipeline([('classifiers', estimator)])
+def run_pipeline(X, Y, pipeline, n_folds, seed):
     accuracy_scorer = make_scorer(accuracy_score)
     kappa_scorer = make_scorer(cohen_kappa_score)
     cv = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=seed)
-    scores = cross_validate(pipe, X.values, Y.values,
-        cv=cv, n_jobs=1, scoring={'accuracy': accuracy_scorer, 'kappa': kappa_scorer})
+    scores = cross_validate(
+        pipeline, X.values, Y.values, cv=cv, n_jobs=1, scoring={
+            'accuracy': accuracy_scorer, 'kappa': kappa_scorer
+        }
+    )
     err_rate = 1. - np.mean(scores['test_accuracy'])
     kappa = np.mean(scores['test_kappa'])
 
     return (err_rate, kappa)
 
 def get_naive_bayes(X, Y, n_folds, seed):
-    estimator = GaussianNB()
-    return run_pipeline(X, Y, estimator, n_folds, seed)
+    pipeline = Pipeline([('naive_bayes', GaussianNB())])
+    return run_pipeline(X, Y, pipeline, n_folds, seed)
 
 def get_knn_1(X, Y, n_folds, seed):
-    estimator = KNeighborsClassifier(n_neighbors = 1, n_jobs=1)
-    return run_pipeline(X, Y, estimator, n_folds, seed)
+    pipeline = Pipeline([(
+        'knn_1', KNeighborsClassifier(n_neighbors = 1, n_jobs=1)
+    )])
+    return run_pipeline(X, Y, pipeline, n_folds, seed)
 
 def get_decision_stump(X, Y, n_folds, seed):
-    estimator = DecisionTreeClassifier(
-        criterion='entropy', splitter='best', max_depth=1, random_state=seed
-    )
-    return run_pipeline(X, Y, estimator, n_folds, seed)
+    pipeline = Pipeline([(
+        'decision_stump', DecisionTreeClassifier(
+            criterion='entropy', splitter='best', max_depth=1, random_state=seed
+        )
+    )])
+    return run_pipeline(X, Y, pipeline, n_folds, seed)
 
 def get_random_tree(X, Y, depth, n_folds, seed):
-    estimator = DecisionTreeClassifier(
-        criterion='entropy', splitter='random', max_depth=depth,
-        random_state=seed
-    )
-    return run_pipeline(X, Y, estimator, n_folds, seed)
+    pipeline = Pipeline([(
+        'random_tree', DecisionTreeClassifier(
+            criterion='entropy', splitter='random', max_depth=depth,
+            random_state=seed
+        )
+    )])
+    return run_pipeline(X, Y, pipeline, n_folds, seed)
 
 def get_lda(X, Y, n_folds, seed):
-    estimator = LinearDiscriminantAnalysis(solver='lsqr', shrinkage='auto')
-    return run_pipeline(X, Y, estimator, n_folds, seed)
+    pipeline = Pipeline([(
+        'lda', LinearDiscriminantAnalysis(
+            solver='lsqr', shrinkage='auto'
+        )
+    )])
+    return run_pipeline(X, Y, pipeline, n_folds, seed)
