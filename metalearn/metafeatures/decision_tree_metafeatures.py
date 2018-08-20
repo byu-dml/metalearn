40a065d5e285c7f7a84f9970696a09c01d7a1313
Mason Poggemann <masonfp@byu.edu>
2018-08-20 13:08:35 -0600

    refactored decision_tree_metafeatures, fixed level_sizes

--- metalearn/metafeatures/decision_tree_metafeatures.py
+++ metalearn/metafeatures/decision_tree_metafeatures.py
@@ -4,73 +4,79 @@ from sklearn.tree import DecisionTreeClassifier
 
 from metalearn.metafeatures.common_operations import profile_distribution
 
+class DecisionTree(object):
+
+    def __init__ (self, X, Y, seed):
+        self.level_sizes = []
+        self.branch_lengths = []
+        self.positions = []
+        self.tree = self._get_decision_tree(X, Y, seed)
+        self.attributes = [
+                            x for x in 
+                            Counter(self.tree.feature).values() if x != -2
+                           ]
+        self.adjacencies = [
+                            (left, right) for left, right in 
+                            zip(self.tree.children_left, 
+                                self.tree.children_right)
+                            ]
+        
+        self._get_general_info()
+        self._traverse_tree(0, 0, 0, 0)
+        self.width = abs(min(self.positions)) + abs(max(self.positions))
+
+    def _get_decision_tree(self, X, Y, seed):
+        estimator = DecisionTreeClassifier(random_state=seed)
+        estimator.fit(X, Y)
+        return estimator.tree_
+
+    def _get_general_info(self):
+        self.n_nodes = self.tree.node_count
+        self.tree_height = self.tree.max_depth + 1
+        self.level_sizes = [0] * (self.tree_height)
+        counts = Counter(self.tree.feature)
+        self.leaf_count = counts[-2]
+
+    def _traverse_tree(
+        self, curr_node, curr_position, curr_level, curr_branch_length
+    ):
+        self.level_sizes[curr_level] += 1
+        if self.adjacencies[curr_node][0] == self.adjacencies[curr_node][1]:
+            self.positions.append(curr_position)
+            self.branch_lengths.append(curr_branch_length)
+        else:
+            self._traverse_tree(
+                self.adjacencies[curr_node][0], curr_position-1, curr_level+1, 
+                curr_branch_length+1
+            )
+            self._traverse_tree(
+                self.adjacencies[curr_node][1], curr_position+1, curr_level+1, 
+                curr_branch_length+1
+            )
+
+
 def get_decision_tree(X, Y, seed):
-    estimator = DecisionTreeClassifier(random_state=seed)
-    estimator.fit(X, Y)
-    return (estimator.tree_,)
+    return (DecisionTree(X, Y, seed),)
 
 
 def get_decision_tree_width(tree):
-    adjacencies = [(left, right) for left, right in zip(tree.children_left, tree.children_right)]
-    positions = _traverse_tree(0, 0, adjacencies, [])
-    return (abs(min(positions)) + abs(max(positions)),)
-
-
-def _traverse_tree(curr_node, curr_position, adjacencies, positions):
-    if adjacencies[curr_node][0] == adjacencies[curr_node][1]:
-        positions.append(curr_position)
-    else:
-        _traverse_tree(
-            adjacencies[curr_node][0], curr_position-1, adjacencies, positions
-        )
-        _traverse_tree(
-            adjacencies[curr_node][1], curr_position+1, adjacencies, positions
-        )
-    return positions
+    return (tree.width,)
 
 
 def get_decision_tree_level_sizes(tree):
-    adjacencies = [(left, right) for left, right in zip(tree.children_left, tree.children_right)]
-    curr_level = [0]
-    level_sizes = [1]
-    while len(curr_level) > 0:
-        next_level = []
-        for node in curr_level:
-            if adjacencies[node][0] != adjacencies[node][1]:
-                next_level.append(adjacencies[node][0])
-                next_level.append(adjacencies[node][1])
-        level_sizes.append(len(next_level))
-        curr_level = next_level
-    level_mean, level_stdev, level_min, _, _, _, level_max = profile_distribution(level_sizes)
+    level_mean, level_stdev, level_min, _, _, _, level_max = profile_distribution(tree.level_sizes)
     return (level_mean, level_stdev, level_min, level_max)
 
 
 def get_decision_tree_branch_lengths(tree):
-    adjacencies = [(left, right) for left, right in zip(tree.children_left, tree.children_right)]
-    leaves = [node for node, children in enumerate(adjacencies) if children[0] == children[1]]
-    branch_lengths = []
-    for leaf in leaves:
-        curr_node = leaf
-        length = 0
-        while curr_node != 0:
-            parent_node = [parent for parent, children in enumerate(adjacencies) if curr_node in children][0]
-            length += 1
-            curr_node = parent_node
-        branch_lengths.append(length)
-    branch_mean, branch_stdev, branch_min, _, _, _, branch_max = profile_distribution(branch_lengths)
+    branch_mean, branch_stdev, branch_min, _, _, _, branch_max = profile_distribution(tree.branch_lengths)
     return (branch_mean, branch_stdev, branch_min, branch_max)
 
 
 def get_decision_tree_attributes(tree):
-    counts = Counter(tree.feature)
-    att_counts = [x for x in counts.values() if x != -2]
-    att_mean, att_stdev, att_min, _, _, _, att_max = profile_distribution(att_counts)
+    att_mean, att_stdev, att_min, _, _, _, att_max = profile_distribution(tree.attributes)
     return (att_mean, att_stdev, att_min, att_max)
 
 
 def get_decision_tree_general_info(tree):
-    n_nodes = tree.node_count
-    tree_height = tree.max_depth + 1
-    counts = Counter(tree.feature)
-    leaf_count = counts[-2]
-    return (n_nodes, tree_height, leaf_count)
+    return (tree.n_nodes, tree.leaf_count, tree.tree_height, tree.width)
