4247173d60a002fd6fc7a5225f939275bd1d0010
Seth Donaldson <poolguy@hal.aml.cs.byu.edu>
2018-12-05 13:59:04 -0700

    get_string_length_means working

--- metalearn/metafeatures/text_metafeatures.py
+++ metalearn/metafeatures/text_metafeatures.py
@@ -12,10 +12,14 @@ from sklearn.cross_decomposition import CCA
 from .common_operations import *
 
 
-def get_string_lengths_array_from_text_features(text_features_array):
-	lengths = [len(feature) for feature in text_features_array]
-	return lengths
+def get_string_length(string):
+	return len(string)
 
+def get_string_lengths_array_from_text_features(text_features_array):
+	lengths = []
+	for feature in text_features_array:
+		lengths.append(feature.apply(get_string_length))
+	return lengths
 
 def get_string_length_means(text_features_array):
 	string_lengths_array = get_string_lengths_array_from_text_features(text_features_array)
