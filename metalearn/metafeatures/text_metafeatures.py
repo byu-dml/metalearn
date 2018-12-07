739cf4cf4c20cfd7b198b38722c9684517c104b9
Seth Donaldson <poolguy@hal.aml.cs.byu.edu>
2018-12-07 15:35:07 -0700

    most_common_tokens_split_by_space

--- metalearn/metafeatures/text_metafeatures.py
+++ metalearn/metafeatures/text_metafeatures.py
@@ -11,6 +11,9 @@ from sklearn.cross_decomposition import CCA
 
 from .common_operations import *
 
+"""
+text_features_array is a list of pd.series objects
+"""
 
 def get_string_length(string):
 	return len(string)
@@ -41,3 +44,65 @@ def get_string_length_kurtosis(text_features_array):
 	string_lengths_array = get_string_lengths_array_from_text_features(text_features_array)
 	kurtoses = [feature.kurtosis() for feature in string_lengths_array]
 	return profile_distribution(kurtoses)
+
+def get_most_common_tokens_split_by_space(text_features_array, most_common_limit=10):
+	tokens = {}
+	alnum_tokens = {}
+	num_tokens = {}
+	# Get dictionary of token:count
+	for feature in text_features_array:
+		for text in feature:
+			for token in text.split(" "):
+				if token.isdigit():
+					if token not in num_tokens:
+						num_tokens[token] = 0
+					num_tokens[token] += 1
+				elif token.isalnum():
+					if token not in alnum_tokens:
+						alnum_tokens[token] = 0
+					alnum_tokens[token] += 1
+
+				if token not in tokens:
+					tokens[token] = 0
+				tokens[token] += 1
+
+	# get most_common_alnum_tokens
+	most_common_alnum_tokens = []
+	for token in sorted(alnum_tokens, key=alnum_tokens.get, reverse=True):
+		most_common_alnum_tokens.append(
+			{
+				"token": token,
+				"count": alnum_tokens[token],
+				"ratio": alnum_tokens[token]/len(tokens)
+			}
+		)
+		if len(most_common_alnum_tokens) == most_common_limit:
+			break
+
+	# get most_common_num_tokens
+	most_common_num_tokens = []
+	for token in sorted(num_tokens, key=num_tokens.get, reverse=True):
+		most_common_num_tokens.append(
+			{
+				"token": token,
+				"count": num_tokens[token],
+				"ratio": num_tokens[token]/len(tokens)
+			}
+		)
+		if len(most_common_num_tokens) == most_common_limit:
+			break
+
+	# get most_common_tokens
+	most_common_tokens = []
+	for token in sorted(tokens, key=tokens.get, reverse=True):
+		most_common_tokens.append(
+			{
+				"token": token,
+				"count": tokens[token],
+				"ratio": tokens[token]/len(tokens)
+			}
+		)
+		if len(most_common_tokens) == most_common_limit:
+			break
+
+	return most_common_tokens, most_common_alnum_tokens, most_common_num_tokens
