2a72f2f3371dc8fcb4ec0e695f5ab47a7d88ad3e
Seth Donaldson <poolguy@hal.aml.cs.byu.edu>
2019-01-09 12:41:29 -0700

    Added StringLengthsArrayFromTextFeatures to metafeatures.json resources

--- metalearn/metafeatures/text_metafeatures.py
+++ metalearn/metafeatures/text_metafeatures.py
@@ -11,33 +11,36 @@ from sklearn.cross_decomposition import CCA
 
 from .common_operations import *
 
-
-def get_string_length(string):
-	return len(string)
-
 def get_string_lengths_array_from_text_features(text_features_array):
 	lengths = []
 	for feature in text_features_array:
-		lengths.append(feature.apply(get_string_length))
+		lengths.append(feature.apply(len))
 	return lengths
 
-def get_string_length_means(text_features_array):
-	string_lengths_array = get_string_lengths_array_from_text_features(text_features_array)
+# string_lengths_array should be of type: list<Series>
+# However, if the length of the list is 1, metafeatures.py will return the resource as a Series (rather than a list of
+# series). This method is added to account for this case. todo: verify that this only happens when the list is of size 1
+def retype_string_lengths_array_if_necessary(string_lengths_array):
+	if type(string_lengths_array) is pd.core.series.Series:
+		string_lengths_array = [string_lengths_array]
+	return string_lengths_array
+
+def get_string_length_means(string_lengths_array):
+	string_lengths_array = retype_string_lengths_array_if_necessary(string_lengths_array)
 	means = [feature.mean() for feature in string_lengths_array]
 	return profile_distribution(means)
 
-def get_string_length_stdev(text_features_array):
-	string_lengths_array = get_string_lengths_array_from_text_features(text_features_array)
+def get_string_length_stdev(string_lengths_array):
+	string_lengths_array = retype_string_lengths_array_if_necessary(string_lengths_array)
 	stdevs = [feature.std() for feature in string_lengths_array]
 	return profile_distribution(stdevs)
 
-
-def get_string_length_skewness(text_features_array):
-	string_lengths_array = get_string_lengths_array_from_text_features(text_features_array)
+def get_string_length_skewness(string_lengths_array):
+	string_lengths_array = retype_string_lengths_array_if_necessary(string_lengths_array)
 	skews = [feature.skew() for feature in string_lengths_array]
 	return profile_distribution(skews)
 
-def get_string_length_kurtosis(text_features_array):
-	string_lengths_array = get_string_lengths_array_from_text_features(text_features_array)
+def get_string_length_kurtosis(string_lengths_array):
+	string_lengths_array = retype_string_lengths_array_if_necessary(string_lengths_array)
 	kurtoses = [feature.kurtosis() for feature in string_lengths_array]
 	return profile_distribution(kurtoses)
