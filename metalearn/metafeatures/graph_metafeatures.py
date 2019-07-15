02b04b9b7e4c6a6e3758a17ebe9de2db7739b795
Mason Poggemann <masonfp@byu.edu>
2019-07-15 11:13:13 -0600

    added function stubs for future reference

---
+++ metalearn/metafeatures/graph_metafeatures.py
@@ -0,0 +1,41 @@
+import numpy as np
+from sklearn.neighbors import RadiusNeighborsClassifier
+
+
+def normalized_diff(a, b):
+    pass
+
+
+def normalized_vdm(a, b):
+    pass
+
+
+def HVDM(a, b):
+    pass
+
+
+def get_fitted_rnn(X, Y, seed):
+    pass
+
+
+def get_rnn_graph(rnn, radius):
+    return rnn.radius_neighbors_graph(radius=radius)
+
+
+def get_number_of_edges(graph):
+    pass
+
+
+def get_path_lengths(graph):
+    pass
+
+def get_clusterings(graph):
+    pass
+
+
+def get_degrees(path):
+    pass
+
+
+def get_class_changes(graph):
+    pass
