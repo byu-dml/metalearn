1f12aadb0a16a77a073b65390fde428bd6390fcb
Brandon Schoenfeld <bjschoenfeld@gmail.com>
2019-08-05 17:32:44 -0600

    recovered new test dataset

--- run_tests.py
+++ run_tests.py
@@ -22,5 +22,5 @@ if __name__ == '__main__':
     if not runner.run(tests).wasSuccessful():
         sys.exit(1)
 
-    # Compute Metafeatures
+    # # Compute Metafeatures
     # compute_dataset_metafeatures()
